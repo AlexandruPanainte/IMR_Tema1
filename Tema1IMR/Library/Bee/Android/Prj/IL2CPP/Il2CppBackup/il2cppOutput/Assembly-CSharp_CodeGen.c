b92633044940ec466f8dccece2e491ec35546d90
AlexandruPanainte <sculap1234321@gmail.com>
2022-10-17 19:18:18 +0300

    Tema1IMR

---
+++ Tema1IMR/Library/Bee/Android/Prj/IL2CPP/Il2CppBackup/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,61 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void ARTapToPlace::Awake()
+extern void ARTapToPlace_Awake_mE71E74CEEAD0549766EAD160FF28F37987FFBF5A (void);
+// 0x00000002 System.Boolean ARTapToPlace::TryGetTouchPosition(UnityEngine.Vector2&)
+extern void ARTapToPlace_TryGetTouchPosition_m771AD2867B95FE51CB1E3784FB4521B18A803FAF (void);
+// 0x00000003 System.Void ARTapToPlace::Update()
+extern void ARTapToPlace_Update_m0FDBA559D7ABA80E790C317924E9B25FFD724D05 (void);
+// 0x00000004 System.Void ARTapToPlace::.ctor()
+extern void ARTapToPlace__ctor_mDD8E97029B4DDB0187EDA173CA4879148DE758CE (void);
+// 0x00000005 System.Void ARTapToPlace::.cctor()
+extern void ARTapToPlace__cctor_m123D2531553B8B60CC6185AE026055A6E3E904B0 (void);
+static Il2CppMethodPointer s_methodPointers[5] = 
+{
+	ARTapToPlace_Awake_mE71E74CEEAD0549766EAD160FF28F37987FFBF5A,
+	ARTapToPlace_TryGetTouchPosition_m771AD2867B95FE51CB1E3784FB4521B18A803FAF,
+	ARTapToPlace_Update_m0FDBA559D7ABA80E790C317924E9B25FFD724D05,
+	ARTapToPlace__ctor_mDD8E97029B4DDB0187EDA173CA4879148DE758CE,
+	ARTapToPlace__cctor_m123D2531553B8B60CC6185AE026055A6E3E904B0,
+};
+static const int32_t s_InvokerIndices[5] = 
+{
+	2075,
+	1155,
+	2075,
+	2075,
+	3842,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	5,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
